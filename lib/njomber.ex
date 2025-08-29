4d89a21d9f7f3da1e6f625296e808f2e10d61656
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-08-29 19:37:24 +0200

    wip commit

--- lib/njomber.ex
+++ lib/njomber.ex
@@ -9,6 +9,23 @@ defmodule Njomber do
 
   alias Anoma.Util
   alias Anoma.Arm.NullifierKey
+  alias Anoma.Arm
+  alias Anoma.Arm.ComplianceUnit
+  alias Anoma.Arm.ComplianceWitness
+  alias Anoma.Arm.ComplianceInstance
+  alias Anoma.Arm.MerklePath
+  alias Anoma.Arm.NullifierKey
+  alias Anoma.Arm.Transaction
+  alias Anoma.Arm.DeltaWitness
+  alias Anoma.Arm.Action
+  alias Anoma.Arm.Resource
+  alias Anoma.Examples.Counter
+  alias Anoma.Examples.Counter.CounterLogic
+  alias Anoma.Examples.Counter.CounterWitness
+  alias Anoma.Arm.MerkleTree
+  alias Anoma.Util
+
+  import Anoma.Util
 
   @doc """
   Create a nullifier and commitment for a new user.
@@ -17,5 +34,33 @@ defmodule Njomber do
   def create_keypair do
     {{nullifier}, {nullifier_commitment}} = NullifierKey.random_pair()
     {Util.binlist2bin(nullifier), Util.binlist2bin(nullifier_commitment)}
+  end
+
+  @doc """
+  Create a new ephemeral counter.
+
+  A counter is represented by its owner, and a unique label.
+  """
+  @spec create_ephemeral_counter(NullifierKey.t(), NullifierKeyCommitment.t()) :: Resource.t()
+  def create_ephemeral_counter(key, commitment) do
+    # the counter value is little endian encoded, padded to 32 bytes.
+    counter_value =
+      0
+      |> :binary.encode_unsigned(:little)
+      |> Util.pad_bitstring(32)
+      |> Util.bin2binlist()
+
+    # Create a counter resource
+    resource = %Resource{
+      logic_ref: Counter.counter_logic_ref(),
+      label_ref: randombinlist(32),
+      quantity: 1,
+      value_ref: counter_value,
+      is_ephemeral: true,
+      nonce: Util.randombinlist(32),
+      nk_commitment: commitment
+    }
+
+    {resource, key}
   end
 end
--- lib/njomber_web/live/counter_live.ex
+++ lib/njomber_web/live/counter_live.ex
@@ -2,14 +2,21 @@ defmodule NjomberWeb.CounterLive do
   use NjomberWeb, :live_view
 
   def mount(_params, _session, socket) do
+    # start with the default keypair
+    keypair = %{
+      nullifier_key: Application.get_env(:njomber, :nullifier_key),
+      nullifier_key_commitment: Application.get_env(:njomber, :nullifier_key)
+    }
+
     socket =
       socket
       |> assign(:loading, false)
-      |> assign(:current_keypair, nil)
+      |> assign(:current_keypair, keypair)
       |> assign(
         :keypair_form,
         to_form(%{"nullifier_key" => "", "nullifier_key_commitment" => ""}, as: :keypair)
       )
+      |> assign(:json_data, %{})
 
     {:ok, socket}
   end
@@ -48,7 +55,7 @@ defmodule NjomberWeb.CounterLive do
   def handle_event("default-keypair", _params, socket) do
     keypair = %{
       nullifier_key: Application.get_env(:njomber, :nullifier_key),
-      nullifier_key_commitment: Application.get_env(:njomber, :nullifier_key),
+      nullifier_key_commitment: Application.get_env(:njomber, :nullifier_key)
     }
 
     socket =
@@ -59,6 +66,7 @@ defmodule NjomberWeb.CounterLive do
     {:noreply, socket}
   end
 
+  # update the keypair based on the user inputs
   def handle_event("update-keypair", %{"keypair" => keypair_params}, socket) do
     keypair = %{
       nullifier_key: keypair_params["nullifier_key"],
@@ -73,111 +81,18 @@ defmodule NjomberWeb.CounterLive do
     {:noreply, socket}
   end
 
-  # change the keypair based on the inputs
-  def handle_event("set_custom_keypair", %{"keypair" => keypair_params}, socket) do
-    nullifier_key = String.trim(keypair_params["nullifier_key"] || "")
-    nullifier_key_commitment = String.trim(keypair_params["nullifier_key_commitment"] || "")
+  # create a new counter object
+  def handle_event("create-counter", _params, socket) do
+    nullifier = socket.assigns.current_keypair.nullifier_key
+    commitment = socket.assigns.current_keypair.nullifier_key_commitment
 
-    keypair = %{
-      nullifier_key: nullifier_key,
-      nullifier_key_commitment: nullifier_key_commitment,
-      created_at: DateTime.utc_now(),
-      custom: true
-    }
+    {ephemeral_counter, _} =
+      Njomber.create_ephemeral_counter(Base.decode64!(nullifier), Base.decode64!(commitment))
 
     socket =
       socket
-      |> assign(:current_keypair, keypair)
-      |> assign(
-        :keypair_form,
-        to_form(%{"nullifier_key" => "", "nullifier_key_commitment" => ""}, as: :keypair)
-      )
-      |> put_flash(:info, "Custom keypair set successfully!")
+      |> update(:json_data, &Map.put(&1, "ephemeral_counter", Jason.encode!(ephemeral_counter, pretty: true)))
 
     {:noreply, socket}
-  end
-
-  def handle_event("clear_keypair", _params, socket) do
-    socket =
-      socket
-      |> assign(:current_keypair, nil)
-      |> put_flash(:info, "Keypair cleared")
-
-    {:noreply, socket}
-  end
-
-  def render(assigns) do
-    ~H"""
-    <Layouts.app flash={@flash}>
-
-    <!-- Keypair -->
-
-      <div id="keypair-section">
-        <h2 class="text-xl font-semibold flex items-center pb-5">
-          <.icon name="hero-key" class="w-5 h-5 mr-2" /> Keypair
-        </h2>
-
-        <div class="flex flex-col">
-          <div>
-            <.button phx-click="create-keypair">
-              Generate
-            </.button>
-            <.button phx-click="default-keypair">
-              Default
-            </.button>
-          </div>
-        </div>
-      </div>
-
-    <!-- Input own keys -->
-      <div>
-        <.form
-          for={@keypair_form}
-          id="keypair-form"
-          phx-change="validate-keypair"
-          phx-submit="update-keypair"
-        >
-          <label>
-            Nullifier Key
-          </label>
-          <.input field={@keypair_form[:nullifier_key]} type="text" />
-
-          <label>
-            Nullifier Key Commitment
-          </label>
-          <.input field={@keypair_form[:nullifier_key_commitment]} type="text" />
-
-          <.button type="submit">
-            Submit
-          </.button>
-        </.form>
-      </div>
-
-    <!-- Current keys in use -->
-      <div :if={@current_keypair} id="generate-keypair-output">
-        <label>
-          Nullifier Key
-        </label>
-        <.input name="nullifier_key" type="text" value={@current_keypair.nullifier_key} />
-
-        <label>
-          Nullifier Key Commitment
-        </label>
-        <.input
-          name="nullifier_key_commitment"
-          type="text"
-          value={@current_keypair.nullifier_key_commitment}
-        />
-      </div>
-
-
-    <!-- Create counter -->
-      <div id="generate-counter" class="content-around">
-          <.button>
-            Initialize Counter
-          </.button>
-      </div>
-    </Layouts.app>
-    """
   end
 end
